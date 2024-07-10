65fdfee6192918592de84617e342d1cf800e865d
Rinkiller <Rinkiller@mail.ru>
2024-07-10 21:09:59 +0300

    dzgdgzgf

--- main.py
+++ main.py
@@ -5,19 +5,31 @@ app = Flask(__name__)
 
 @app.get('/')
 def root():
-    return render_template('index.html')
+    context = {'zagolovoc':['Это  ГЛАВНАЯ   страница  '],'poem': [
+                        'эта страница идет вперед всех  ',
+                        ' эту страницу в конце ждет успех ']}
+    return render_template('index.html', **context)
 
 @app.get('/page-1/')
 def page1():
-    return render_template('odezjda.html')
+    context = {'zagolovoc': ['ОБУВЬ'],'poem': [
+                        'бла бла бла  ',
+                        ' про обувь ']}
+    return render_template('index.html', **context)
 
 @app.get('/page-2/')
 def page2():
-    return render_template('obuv.html')
+    context = {'zagolovoc': ['Одежда'],'poem': [
+                        'бла бла бла  ',
+                        ' про одежду ']}
+    return render_template('index.html', **context)
 
 @app.get('/page-3/')
 def page3():
-    return render_template('kurtka.html')
+    context = {'zagolovoc': ['Куртка'],'poem': [
+                        'бла бла бла  ',
+                        ' про куртки ']}
+    return render_template('index.html', **context)
 
 if __name__=='__main__':
     app.run(debug=True)
